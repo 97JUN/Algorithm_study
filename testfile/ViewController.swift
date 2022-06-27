29f0a5181c6853e742dfbf61c73605efb19e6cc2
Hyunjun Lee <90228137+97JUN@users.noreply.github.com>
2022-06-27 14:01:44 +0900

    Initial Commit

---
+++ testfile/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testfile
+//
+//  Created by 쭌이 on 2022/06/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
